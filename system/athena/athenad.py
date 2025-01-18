jixiexiaoge/openpilot

Konik.ai Server support

system/athena/athenad.py
@@ -40,8 +40,10 @@ from openpilot.common.swaglog import cloudlog
 from openpilot.system.version import get_build_metadata
 from openpilot.system.hardware.hw import Paths
 
+from openpilot.selfdrive.frogpilot.frogpilot_variables import get_frogpilot_toggles
 
 ATHENA_HOST = os.getenv('ATHENA_HOST', 'wss://athena.comma.ai')
+SPRINGER_ATHENA_HOST = os.getenv('ATHENA_HOST', 'wss://athena.konik.ai')
 HANDLER_THREADS = int(os.getenv('HANDLER_THREADS', "4"))
 LOCAL_PORT_WHITELIST = {8022}
 
@@ -785,7 +787,7 @@ def main(exit_event: threading.Event = None):
   dongle_id = params.get("DongleId", encoding='utf-8')
   UploadQueueCache.initialize(upload_queue)
 
-  ws_uri = ATHENA_HOST + "/ws/v2/" + dongle_id
+  ws_uri = (SPRINGER_ATHENA_HOST if get_frogpilot_toggles().use_konik_server else ATHENA_HOST) + "/ws/v2/" + dongle_id
   api = Api(dongle_id)
 
   conn_start = None

fungab/FrogPilot

system/athena/athenad.py
@@ -40,8 +40,10 @@ from openpilot.common.swaglog import cloudlog
 from openpilot.system.version import get_build_metadata
 from openpilot.system.hardware.hw import Paths
 
+from openpilot.selfdrive.frogpilot.frogpilot_variables import get_frogpilot_toggles
 
 ATHENA_HOST = os.getenv('ATHENA_HOST', 'wss://athena.comma.ai')
+KONIK_ATHENA_HOST = os.getenv('ATHENA_HOST', 'wss://athena.konik.ai')
 HANDLER_THREADS = int(os.getenv('HANDLER_THREADS', "4"))
 LOCAL_PORT_WHITELIST = {8022}
 
@@ -785,7 +787,7 @@ def main(exit_event: threading.Event = None):
   dongle_id = params.get("DongleId", encoding='utf-8')
   UploadQueueCache.initialize(upload_queue)
 
-  ws_uri = ATHENA_HOST + "/ws/v2/" + dongle_id
+  ws_uri = (KONIK_ATHENA_HOST if get_frogpilot_toggles().use_konik_server else ATHENA_HOST) + "/ws/v2/" + dongle_id
   api = Api(dongle_id)
 
   conn_start = None